330c38adae502369fe2c10f06b370bb0fdc00b57
Lucas Gentil Carlos <lgentil@cpqd.com.br>
2024-03-18 09:15:46 -0300

    Loops implementados

--- booking-app/main.go
+++ booking-app/main.go
@@ -6,7 +6,10 @@ package main
 // É necessário um ponto de entrada para que um programa GO se localize
 // 1 Progrmaa só pode ter 1 função "main" por que você só pode ter 1 ponto de entrada
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "GO conference"
@@ -24,40 +27,52 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Printf("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
 
-	fmt.Printf("\nEnter your first name: ")
-	fmt.Scan(&firstName)
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Printf("Enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Printf("\nEnter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Printf("Enter your e-mail: ")
-	fmt.Scan(&email)
+		fmt.Printf("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Printf("Enter your number of tickets: ")
-	fmt.Scan(&userTickets)
+		fmt.Printf("Enter your e-mail: ")
+		fmt.Scan(&email)
 
-	remainingTickets = remainingTickets - userTickets
+		fmt.Printf("Enter your number of tickets: ")
+		fmt.Scan(&userTickets)
 
-	// Em GO para utilizar lista é necessário definir quandos itens poderám ser armazenados nesta lista
-	// E é necessario dizer qual o tipo de dado que terá na lista, ou seja não é possivel utilizar INT e STRING juntos.
-	// bookings[0] = firstName + " " + lastName  -> Exemplo de inserção em 1 lista
-	bookings = append(bookings, firstName+" "+lastName) // Inserindo em nosso slice
+		remainingTickets = remainingTickets - userTickets
 
-	// fmt.Printf("Slice completo: %v\n", bookings)
-	// fmt.Printf("Primeiro valor: %v\n", bookings[0])
-	// fmt.Printf("Tipo do Slice: %T\n", bookings)
-	// fmt.Printf("Tamanho do Slice: %v\n", len(bookings))
+		// Em GO para utilizar lista é necessário definir quandos itens poderám ser armazenados nesta lista
+		// E é necessario dizer qual o tipo de dado que terá na lista, ou seja não é possivel utilizar INT e STRING juntos.
+		// bookings[0] = firstName + " " + lastName  -> Exemplo de inserção em 1 lista
+		bookings = append(bookings, firstName+" "+lastName) // Inserindo em nosso slice
 
-	// Função scanf para pedir informações
-	fmt.Printf("\nObrigado %v %v por comprar %v tickets! Você irá receber a confirmação da compra no seguinte e-mail: %v \n", firstName, lastName, userTickets, email)
-	fmt.Printf("Número de tickets em estoque após a compra: %v", remainingTickets)
+		// fmt.Printf("Slice completo: %v\n", bookings)
+		// fmt.Printf("Primeiro valor: %v\n", bookings[0])
+		// fmt.Printf("Tipo do Slice: %T\n", bookings)
+		// fmt.Printf("Tamanho do Slice: %v\n", len(bookings))
 
-	fmt.Printf("Todos Bookings na aplicação: %v\n", bookings)
+		// Função scanf para pedir informações
+		fmt.Printf("\nObrigado %v %v por comprar %v tickets! Você irá receber a confirmação da compra no seguinte e-mail: %v \n", firstName, lastName, userTickets, email)
+		fmt.Printf("Número de tickets em estoque após a compra: %v", remainingTickets)
+
+		firstNames := []string{}
+		// Definindo for para iterar o slice Bookings, com a função range conseguimos o index e o valor de cada elemento
+		// Tanto para listas ou slices
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+
+		fmt.Printf("\nO primeiro nome dos compradores: %v\n", firstNames)
+
+	}
 }
 
 // Para executar 1 arquivo, executar no terminal: go run <file name> -> Irá compilar e rodar o código
